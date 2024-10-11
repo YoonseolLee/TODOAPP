74f2edb25dc0f1c6af5d8d4b726fc8b095c8f65f
YoonseolLee <thisyoon97@gmail.com>
2024-10-11 11:17:53 +0900

    Alert Dialog - Delete single task

---
+++ app/src/main/java/com/example/todocompose/components/DisplayAlertDialog.kt
@@ -0,0 +1,57 @@
+package com.example.todocompose.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.example.todocompose.R
+
+@Composable
+fun DisplayAlertDialog(
+    title: String,
+    message: String,
+    openDialog: Boolean,
+    closeDialog: () -> Unit,
+    onYesClicked: () -> Unit,
+) {
+    if (openDialog) {
+        AlertDialog(
+            title = {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            text = {
+                Text(
+                    text = message,
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Normal
+                )
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        onYesClicked()
+                        closeDialog()
+                    })
+                {
+                    Text(text = stringResource(id = R.string.yes))
+                }
+            },
+            dismissButton = {
+                OutlinedButton(
+                    onClick = { closeDialog() })
+                {
+                    Text(text = stringResource(id = R.string.no))
+                }
+            },
+            onDismissRequest = { closeDialog() }
+        )
+    }
+}
