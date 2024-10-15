1c681862c05f6b1d5ae8f413f9975ca02f1d2bca
YoonseolLee <thisyoon97@gmail.com>
2024-10-15 12:26:13 +0900

    Animate Splash Screen

--- app/src/main/java/com/example/todocompose/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/splash/SplashScreen.kt
@@ -1,19 +1,30 @@
 package com.example.todocompose.ui.screens.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.todocompose.R
 import com.example.todocompose.ui.theme.LOGO_HEIGHT
 import com.example.todocompose.ui.theme.splashScreenBackground
@@ -24,10 +35,27 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(durationMillis = 1000)
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000)
+    )
+
     LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
+
+    Splash(offsetState = offsetState, alphaState = alphaState)
+}
+
+@Composable
+fun Splash(offsetState: Dp, alphaState: Float) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -36,7 +64,9 @@ fun SplashScreen(
     ) {
         Image(
             modifier = Modifier
-                .size(LOGO_HEIGHT),
+                .size(LOGO_HEIGHT)
+                .offset(y = offsetState)
+                .alpha(alphaState),
             painter = painterResource(id = getLogo()),
             contentDescription = stringResource(id = R.string.to_do_logo)
         )
@@ -44,18 +74,12 @@ fun SplashScreen(
 }
 
 @Composable
-fun getLogo(): Int {
-    return if (isSystemInDarkTheme()) {
-        R.drawable.ic_logo_dark
-    } else {
-        R.drawable.ic_logo_light
-    }
-}
+fun getLogo(): Int =
+    if (isSystemInDarkTheme())
+        R.drawable.ic_logo_dark else R.drawable.ic_logo_light
 
 @Composable
 @Preview
 private fun SplashScreenPreview() {
-    SplashScreen(
-        navigateToListScreen = {}
-    )
+    Splash(offsetState = 0.dp, alphaState = 1f)
 }
