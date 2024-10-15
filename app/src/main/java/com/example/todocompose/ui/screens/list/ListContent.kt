dc95ca5a0289b4c2ac621f9f8a37bdcd0a2f25cc
YoonseolLee <thisyoon97@gmail.com>
2024-10-15 11:01:44 +0900

    Swipe to delete - Animate Item Visibility (Not working)

--- app/src/main/java/com/example/todocompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListContent.kt
@@ -1,5 +1,13 @@
 package com.example.todocompose.ui.screens.list
 
+import android.annotation.SuppressLint
+import android.view.FrameMetrics.ANIMATION_DURATION
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -8,12 +16,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +34,11 @@ import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -32,10 +48,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todocompose.R
 import com.example.todocompose.data.models.Priority
 import com.example.todocompose.data.models.ToDoTask
-import com.example.todocompose.ui.theme.HighPriorityColor
 import com.example.todocompose.ui.theme.LARGE_PADDING
 import com.example.todocompose.ui.theme.PRIORITY_INDICATOR_SIZE
 import com.example.todocompose.ui.theme.taskItemBackgroundColor
@@ -43,7 +59,9 @@ import com.example.todocompose.ui.theme.taskItemTextColor
 import com.example.todocompose.util.Action
 import com.example.todocompose.util.RequestState
 import com.example.todocompose.util.SearchAppBarState
+import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListContent(
     allTasks: RequestState<List<ToDoTask>>,
@@ -97,6 +115,8 @@ fun ListContent(
     }
 }
 
+@ExperimentalMaterial3Api
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HandleListContent(
     tasks: List<ToDoTask>,
@@ -114,59 +134,82 @@ fun HandleListContent(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@ExperimentalAnimationApi
+@ExperimentalMaterial3Api
 @Composable
 fun DisplayTasks(
+    modifier: Modifier = Modifier,
     tasks: List<ToDoTask>,
     onSwipeToDelete: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
     ) {
         items(
             items = tasks,
             key = { task -> task.id }
         ) { task ->
-            val dismissState = rememberSwipeToDismissBoxState(
-                confirmValueChange = { dismissValue ->
-                    if (dismissValue == SwipeToDismissBoxValue.EndToStart) {
-                        onSwipeToDelete(Action.DELETE, task)
-                        true
-                    } else false
+            val dismissState = rememberSwipeToDismissBoxState()
+            val isDismissed = dismissState.currentValue != SwipeToDismissBoxValue.Settled
+
+            LaunchedEffect(dismissState.currentValue) {
+                if (dismissState.currentValue != SwipeToDismissBoxValue.Settled) {
+                    delay(300)
+                    onSwipeToDelete(Action.DELETE, task)
                 }
+            }
+
+            val degrees by animateFloatAsState(
+                if (dismissState.targetValue == SwipeToDismissBoxValue.Settled) 0f else -45f
             )
 
-            SwipeToDismissBox(
-                state = dismissState,
-                backgroundContent = {
-                    val degrees = if (dismissState.targetValue == SwipeToDismissBoxValue.Settled) 0f else -45f
-                    RedBackground(degrees = degrees)
-                },
-                content = {
-                    TaskItem(
-                        toDoTask = task,
-                        navigateToTaskScreen = navigateToTaskScreen
+            var itemAppeared by remember { mutableStateOf(false) }
+            LaunchedEffect(key1 = true) {
+                itemAppeared = true
+            }
+
+            AnimatedVisibility(
+                visible = itemAppeared && !isDismissed,
+                enter = expandVertically(
+                    animationSpec = tween(
+                        durationMillis = ANIMATION_DURATION
                     )
-                },
-                enableDismissFromEndToStart = true
-            )
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(
+                        durationMillis = ANIMATION_DURATION
+                    )
+                )
+            ) {
+                SwipeToDismissBox(
+                    state = dismissState,
+                    enableDismissFromStartToEnd = false,
+                    enableDismissFromEndToStart = true,
+                    backgroundContent = { RedBackground(degrees = degrees) },
+                    content = {
+                        TaskItem(
+                            toDoTask = task,
+                            navigateToTaskScreen = navigateToTaskScreen
+                        )
+                    }
+                )
+            }
         }
     }
 }
-
 
 @Composable
 fun RedBackground(degrees: Float) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(HighPriorityColor)
-            .padding(horizontal = LARGE_PADDING),
+            .background(Color.Red)
+            .padding(horizontal = 24.dp),
         contentAlignment = Alignment.CenterEnd
     ) {
         Icon(
-            modifier = Modifier.rotate(degrees = degrees),
+            modifier = Modifier.rotate(degrees),
             imageVector = Icons.Filled.Delete,
             contentDescription = stringResource(id = R.string.delete_icon),
             tint = Color.White
@@ -240,4 +283,12 @@ fun TaskItemPreview() {
         ),
         navigateToTaskScreen = {}
     )
+}
+
+@Composable
+@Preview
+private fun RedbackgroundPreview() {
+    Column(modifier = Modifier.height(100.dp)) {
+        RedBackground(degrees = 0f)
+    }
 }
